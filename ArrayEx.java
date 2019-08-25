341ea9207835be68b35e6ca3cb9eed0cedbd7779
wjh <feiyunchuxiu@163.com>
2019-08-25 18:20:49 +0800

    数组的初始化

---
+++ ArrayEx.java
@@ -0,0 +1,14 @@
+package com.wjh;
+
+import javax.swing.*;
+
+public class ArrayEx {
+    public static void main(String[] args) {
+
+        String []arg=new String[10];
+        String[] params={"1","2"};
+        String[] params1=new String[]{"1","2"};
+
+
+    }
+}
