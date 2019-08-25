3269dbf62cfbcee382f74f64506c9fd616974086
wjh <feiyunchuxiu@163.com>
2019-08-25 17:02:03 +0800

    sysytem.in

---
+++ specConvert.java
@@ -0,0 +1,29 @@
+package com.wjh;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class specConvert {
+
+    public static void main(String[] args) {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String s = br.readLine();
+            System.out.println(s);
+            String s1 = br.readLine();
+            System.out.println(s1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
+
+
+}
